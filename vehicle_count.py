0a65a5935566cb5fa8b95d87f982049bee89a0a5
Nanda <63295747+whitedevil31@users.noreply.github.com>
2022-04-10 22:49:11 +0530

    Update vehicle_count.py

--- vehicle_count.py
+++ vehicle_count.py
@@ -6,7 +6,7 @@ import collections
 import numpy as np
 from tracker import *
 
-# Initialize Tracker
+# Initializing the Tracker
 tracker = EuclideanDistTracker()
 
 # Initialize the videocapture object
